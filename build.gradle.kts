158653a582e31e9f7a5f38f97a6b850d3f0c2edd
chippmann <cedric.hippmann@hotmail.com>
2024-02-08 20:47:07 +0100

    Update godot kotlin jvm version and use gradle version catalog

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,10 @@
 plugins {
-    id("com.utopia-rise.godot-kotlin-jvm") version "0.7.2-4.1.2"
+    alias(libs.plugins.godot.kotlin.jvm)
     `maven-publish`
 }
 
 group = "ch.hippmann.godot"
-version = "0.0.1"
+version = libs.versions.godot.kotlin.jvm.utilities.get()
 
 repositories {
     mavenCentral()
@@ -17,12 +17,12 @@ godot {
 }
 
 dependencies {
-    compileOnly("com.utopia-rise:godot-library:0.7.2-4.1.2")
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.3")
+    compileOnly(libs.godot.kotlin.jvm)
+    implementation(libs.kotlinx.coroutines.core)
 }
 
 kotlin {
-    jvmToolchain(17)
+    jvmToolchain(libs.versions.jvmToolchainVersion.get().toInt())
 }
 
 java {
