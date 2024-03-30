93f3175eb786ead5f0f6bb7346b99f5649e96020
chippmann <cedric.hippmann@hotmail.com>
2024-03-30 17:32:34 +0100

    Add release link

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -16,6 +16,7 @@ dependencyResolutionManagement {
 }
 
 plugins {
+    // https://plugins.gradle.org/plugin/org.gradle.toolchains.foojay-resolver-convention
     id("org.gradle.toolchains.foojay-resolver-convention") version "0.8.0"
 }
 
